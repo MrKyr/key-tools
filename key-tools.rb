9a88abb5c340c87e6650dcc5dcd9f78d844bc303
Kyriakos Bellios <kyr@designisdesign.eu>
2021-09-21 17:07:39 +0300

    * Fixed error: id3v2: invalid option -- R * Added support 'bsd sed' for mac * Added custom 'Homebrew' formula `key-tools.rb`
      Install with:
        $ brew install --HEAD ./key-tools.rb

---
+++ key-tools.rb
@@ -0,0 +1,30 @@
+class KeyTools < Formula
+  desc "Musical key analysis tools"
+  homepage 'http://www.pogo.org.uk/~mark/key-tools/'
+  head "https://www.pogo.org.uk/~mark/key-tools.git", branch: "master"
+  license "GPL-3.0"
+  version '0.1'
+
+  depends_on "fftw"
+
+  patch :DATA
+
+  def install
+    system "make"
+    bin.install "key"
+    bin.install "key-tag"
+  end
+end
+
+__END__
+--- a/key-tag
++++ b/key-tag
+@@ -65,7 +65,7 @@ case "$FILE" in
+ 	KEY=`metaflac --show-tag=KEY "$FILE" | sed -e 's/KEY=//'`
+ 	;;
+ *.mp3)
+-	KEY=`id3v2 -R "$FILE" | sed -n 's/^TKEY.*: \([0-9\.]\+\)/\1/p'`
++	KEY=`id3v2 -l "$FILE" | sed -En 's/^TKEY.*: ([0-9\.]+)/\1/p'`
+ 	;;
+ *.ogg)
+ 	KEY=`vorbiscomment "$FILE" | sed -n 's/^KEY=//p'`
